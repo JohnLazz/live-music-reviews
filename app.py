44de3d1e404ab9a122f707374d5048c437e33c2b
JohnLazz <johnlazzari1@gmail.com>
2022-04-17 13:32:10 +0000

    Add register.html and functionality

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,30 @@ mongo = PyMongo(app)
 def get_reviews():
     reviews = mongo.db.reviews.find()
     return render_template("reviews.html", reviews=reviews)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check for existing username
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username taken, try something else")
+            return redirect(url_for('register'))
+
+        new_user = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(
+                request.form.get("password").lower())
+        }
+        mongo.db.users.insert_one(new_user)
+
+        # create session cookie
+        session["current_user"] = request.form.get("username").lower()
+        flash("Great, your new account is good to go!")
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
